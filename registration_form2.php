aad4ff6ff29afe275ffbb6e1b5e052afe3f3728a
Sheldon Allison <sheldonallison50@gmail.com>
2023-05-01 15:27:31 -0400

    Add files via upload

---
+++ registration_form2.php
@@ -0,0 +1,17 @@
+<?php
+file_exists($filename); 
+?>
+
+<?php
+if (file_exists('my_settings.txt'))
+ {    
+        echo 'file found!';
+ } 
+else
+ {     
+        echo 'my_settings.txt does not exist';
+  } 
+?>
+<?php
+fopen($file_name,$mode,$use_include_path,$context);
+ ?>
